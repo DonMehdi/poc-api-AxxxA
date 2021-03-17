6391737b2b9c15fe73c01065190323a2ac082e93
Mehdi <mehdi.koraichi@gmail.com>
2021-03-17 20:06:00 +0100

    [Feature-005] Create Http Client

--- middlewares/error.js
+++ middlewares/error.js
@@ -6,33 +6,32 @@ const {
   ForbiddenError,
   ValidationError,
   InternalServorError,
-} = require("../utils/apiError")
+} = require("../utils/apiError");
 
-module.exports = (err, req, res) => {
-  let { statusCode } = err
-  console.log(err.statusCode)
-  const { code, message } = getRightError(statusCode)
+module.exports = (err, req, res, _next) => {
+  let { statusCode } = err;
+  const { code, message } = getRightError(statusCode, err);
 
   const response = {
     code,
     message,
-  }
-  res.status(response.code).json(response)
-}
+  };
+  res.status(response.code).json(response);
+};
 
-const getRightError = (statusCode) => {
+const getRightError = (statusCode, err) => {
   switch (statusCode) {
     case 401:
-      return new AuthenticationError()
+      return new AuthenticationError();
     case 404:
-      return new NotFoundError()
+      return new NotFoundError();
     case 400:
-      return new BadRequestError()
+      return new BadRequestError();
     case 403:
-      return new ForbiddenError()
+      return new ForbiddenError();
     case 422:
-      return new ValidationError()
+      return new ValidationError();
     default:
-      return new InternalServorError()
+      return new InternalServorError(err.message);
   }
-}
+};
